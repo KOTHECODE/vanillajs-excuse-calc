b260e923f900b801aab66689b66fd9fcff95e7a9
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-03-26 21:05:16 +0000

    Project Complete

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 
-document.getElementsByClassName("Qbtn").onclick = function () {
+function generateExcuse() {
+    
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-let excuse = Math.floor(Math.random()*4);
-return who[excuse] + ' ' + action[excuse] + ' ' + what[excuse] + ' ' + when[excuse] + ' ' +;
-}
-console.log("return")
+    let arrayposition = Math.floor(Math.random() * 3)
+    let fullSentence = `${who[arrayposition]} ${action[arrayposition]} ${what[arrayposition]} ${when[arrayposition]}`;
+    let span = document.getElementById("excuse");
+    span.innerHTML = fullSentence;
+  }
+
+
