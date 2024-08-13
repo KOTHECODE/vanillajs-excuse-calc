2ceb62e7cca291598f0f44943558ae48734a15db
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-08-13 00:18:12 +0000

    new errors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function phrase() {
+    let who = ["the dog", "my leg", "uncle", "The Traffic"];
+    let what = ["slowed", "jammed up", "got shakey", "did not pick up"];
+    let when = [
+      "even though  i  was late",
+      "right in time",
+      "when I need it the most",
+      "during my down time",
+      "while I was so far"
+    ];
+
+    let part = Math.floor(Math.random() * 4);
+
+    return who[part] + " " + what[part] + " " + when[part];
+  }
+
+  document.textContent = "<h1>Sorry I'm late but,  " + phrase() + "</h1>";
 };
